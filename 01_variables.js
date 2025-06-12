699ccad87fce518cae1cbe91ef2a9357ead057b9
payal12-PM <payalku0305@gmail.com>
2025-06-12 04:48:00 +0000

    dtattype

---
+++ 01_variables.js
@@ -0,0 +1,59 @@
+let accountId = 12345;
+let accountEmail = "payal@example.com";
+let accountPassword = "secure123";
+let accountCity = "Jaipur"; // <-- fix is here
+let accountState;
+
+
+
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+// console.log(accountState);
+
+
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity="Bengaluru"
+
+
+
+// if (condition){
+// }
+
+
+/*
+  Prefer not to use var 
+  because of issue in back scope and function*/
+  
+
+
+
+
+
+
+
+
+
+
+console.table({
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
